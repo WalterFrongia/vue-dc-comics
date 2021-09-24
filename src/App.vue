3177c89e6e43d7656814e106f9bd63f02f18017c
WalterFrongia <walter.frongia@libero.it>
2021-09-24 17:31:16 +0200

    Import di variable.scss

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,9 @@ export default {
 </script>
 
 <style lang="scss">
+
+@import "./style/variables.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
