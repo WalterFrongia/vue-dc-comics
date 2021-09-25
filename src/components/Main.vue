7946f21cc6ba581c216f30173fd7c77df65e3b96
WalterFrongia <walter.frongia@libero.it>
2021-09-25 14:52:38 +0200

    Importato file con variables.scss e aggiunte img nel file images

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,7 @@
 <template>
-    <div></div>
+    <div>
+       <p>ciao</p>
+    </div>
 </template>
 
 <script>
@@ -8,6 +10,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
+@import "../style/variables.scss" scoped;
 
 </style>
