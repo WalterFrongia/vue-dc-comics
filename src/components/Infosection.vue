67a3abcb0bda72ae293d67afc3f2bd5c78acbffd
WalterFrongia <walter.frongia@libero.it>
2021-09-25 16:24:41 +0200

    Implementata con contenuto template e style file main e creato file infosection

---
+++ src/components/Infosection.vue
@@ -0,0 +1,18 @@
+vue.<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "../style/variables.scss";
+
+
+</style>
