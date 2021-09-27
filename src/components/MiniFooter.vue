cb278d02f7a0175ae9511559f2bbc6d4011d6377
WalterFrongia <walter.frongia@libero.it>
2021-09-27 16:04:21 +0200

    Implementazione layout miniFooter con aggiunta di img social e button

--- src/components/MiniFooter.vue
+++ src/components/MiniFooter.vue
@@ -1,8 +1,19 @@
 <template>
 
     <div id="mini-footer-section">
-        <div id="container-elements-margin">
+        <div id="container-elements-margin" class="flex-container">
+            <div id="button-section">
+                <button>SIGN-UP NOW!</button>
+            </div>
 
+            <div id="social-section">
+                <h3>FOLLOW US</h3>
+                <img src="../assets/images/footer-facebook.png" alt="">
+                <img src="../assets/images/footer-twitter.png" alt="">
+                <img src="../assets/images/footer-youtube.png" alt="">
+                <img src="../assets/images/footer-pinterest.png" alt="">
+                <img src="../assets/images/footer-periscope.png" alt="">
+            </div>
         </div>
 
     </div>
@@ -32,4 +43,34 @@ export default {
 }
 //FINE MARGIN E RESET
 
+#mini-footer-section{
+    background-color: rgb(61, 60, 60);
+
+    padding:40px 0;
+}
+
+button{
+    padding:20px 30px;
+    background-color: transparent;
+    border:3px solid rgb(40, 88, 190);
+    color:white;
+}
+
+.flex-container{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+#social-section{
+    display: flex;
+    align-items: center;
+    h3{
+        padding-right: 10px;
+        color:rgb(40, 88, 190);
+    }
+    img{
+        padding:0 5px;
+    }
+}
 </style>
