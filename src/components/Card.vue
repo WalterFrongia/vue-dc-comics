468e015ae4d7b91edaa7fe2787cbcdc1812f3d9e
WalterFrongia <walter.frongia@libero.it>
2021-09-27 23:37:20 +0200

    Implementazione delle cards tramite componente card con js in content

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card-element">
+        <img :src="image" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["image"],
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "../style/variables.scss";
+@import "../style/general.scss";
+
+.card-element{
+    // height: 150px;
+    background-color: rgb(92, 92, 202);
+    margin:5px;
+
+    flex-basis: calc( 100% / 6 - 12px);
+}
+
+img{
+    height: 80%;
+    width:80%;
+}
+
+</style>
