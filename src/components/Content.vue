ffef150fd0bdd6d2a6737417c41a29abc25f374f
WalterFrongia <walter.frongia@libero.it>
2021-09-27 22:19:54 +0200

    Modifica della components Content, con aggiunta di id jumbotron e id conent section per aggiunta delle cards

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,8 +1,13 @@
 <template>
-    <div id="content-section">
-       <div id="container-elements-margin">
-           <h1></h1>
-       </div>
+    <div>
+        <div id="jumbotron-section">
+            <div id="container-elements-margin">
+            <button>CURRENT SERIES</button>
+            </div>
+        </div>
+        <div id="container-content">
+
+        </div>
     </div>
 </template>
 
@@ -28,11 +33,29 @@ h1{
     color:white;
 }
 
-#content-section{
+#jumbotron-section{
     padding:60px 0;
     background-image: url(../assets/images/jumbotron.jpg);
     background-size: cover;
     background-repeat: no-repeat;
+    height: 350px;
+
+    position:relative;
+    button{
+
+        background-color: $primaryColor;
+        color:white;
+        padding:10px 20px;
+        border:none;
+        
+        position:absolute;
+        bottom:-20px;
+    }
+}
+
+#container-content{
+    height: 300px; //altezza per visualizzare gli spazi, poi va rimossa
+    background-color: rgb(36, 36, 36);
 }
 
 </style>
